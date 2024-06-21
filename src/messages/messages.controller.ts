b55aff0ec5d8be1d85a57dbbb02f1e0e10cf308d
danstonebuilt <dnansel.dev@gmail.com>
2024-06-20 22:28:22 -0300

    more itens added

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -3,7 +3,14 @@ import { Controller, Get, Post } from '@nestjs/common';
 @Controller('messages')
 export class MessagesController {
   @Get()
-  listMessages() {}
+  listMessages() {
+    return [
+      { id: 1, content: 'Das ist eine message 01' },
+      { id: 2, content: 'Das ist eine message 02' },
+      { id: 3, content: 'Das ist eine message 03' },
+      { id: 4, content: 'Das ist eine message 04' },
+    ];
+  }
 
   @Post()
   createMessage() {}
