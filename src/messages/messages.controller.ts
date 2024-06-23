c76bcdb7dcce463fa4bf564c5e698f90dc38cdf8
danstonebuilt <dnansel.dev@gmail.com>
2024-06-22 22:45:01 -0300

    more itens added

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-messages.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -13,7 +14,7 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
   }
 
